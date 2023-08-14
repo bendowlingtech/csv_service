cf6e424be6caa320b2cab982cd01ab504a6fe8f6
Ben Dowling <bendowlingtech@gmail.com>
2023-08-14 20:26:45 +0100

    added csv import and http pointer

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"encoding/csv"
 )
 
-func handler(w http.ResponseWriter, r http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, my name is %s!", r.URL.Path[1:])
 }
 
+func csvHandler() {
+
+}
+
 func main() {
+	fmt.Printf("Starting server at port 8081")
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/csv", csvHandler)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
 
 }
